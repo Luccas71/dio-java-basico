3370b40f5bc7ceaf35a89a5ba7661d1a2e83cfcb
Lucas Volpato <lmaciel916@gmail.com>
2024-05-08 11:05:24 -0300

    Iphone

---
+++ iphone/iphone/src/Usuario.java
@@ -0,0 +1,17 @@
+import domain.Iphone;
+
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        Iphone iphone = new Iphone();
+
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica();
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        iphone.exibirPagina();
+        iphone.ad
+        iphone.atualizarPagina();
+    }
+}
